876e8a74fc6ff92fc6b403c2a6de1778a934a3eb
KyungSoo <kyung5587@gmail.com>
2018-10-12 15:41:02 +0900

    계수정렬 저장

--- [Al]03_201604140_박경수_Java/src/Algorithm/Counting_Sort.java
+++ [Al]03_201604140_박경수_Java/src/Algorithm/Counting_Sort.java
@@ -9,32 +9,68 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class Counting_Sort {
-	ArrayList<Integer> array = new ArrayList<>();
-	ArrayList<Integer> temp_array = new ArrayList<>();
-	ArrayList<Integer> sorted_array = new ArrayList<>();
+	ArrayList<Integer> array = new ArrayList<>(); //A - 정렬 할 배열
+	ArrayList<Integer> temp_array = new ArrayList<>(); //C - 임시 배열
+	ArrayList<Integer> sorted_array = new ArrayList<>(); //B - 정렬 된 배열
 	
-	Counting_Sort() {
+	/*
+	 * 100줄에 대해 읽어와 Counting_Sort를 진행하고 저장한다.
+	 * 1000줄에 대해서 정렬하기 위해 ArrayList 초기화한다.
+	 * 1000줄에 대해 읽어와 Counting_Sort를 진행하고 저장한다.
+	 */
+	public Counting_Sort() {
+		System.out.println("Counting_Sort is starting..."+ "\n");
 		test100_read();
+		Counting_Sort();
+		test100_save();
+		init_array(); // 1000줄에 대해서 정렬하기 위해 ArrayList 초기화
 		test1000_read();
+		Counting_Sort();
+		test1000_save();
+		System.out.println("Counting_Sort save successful");
+		System.out.println("------------------------------------------------");
 	}
 	
-	public int max() {
-		int temp = array.get(0);
-		for(int i = 1; i < array.size(); i++) {
-			if(temp < array.get(i)) {
-				temp = array.get(i);
+	// ArrayList의 원소 중 가장 큰 값을 찾는 함수
+	public int max(ArrayList<Integer> arr) {
+		int temp = arr.get(0);
+		for(int i = 1; i < arr.size(); i++) {
+			if(temp < arr.get(i)) {
+				temp = arr.get(i);
 			}
 		}
 		return temp;
 	}
 	
+	// ArrayList들을 초기화하는 함수
+	public void init_array() {
+		array.clear();
+		temp_array.clear();
+		sorted_array.clear();
+	}
+	
+	// 계수정렬을 실행하는 함수
 	public void Counting_Sort() {
-		for(int i = 0; i < max(); i++) {
+		// 먼저 sorted_array를 초기화 시킨다.
+		for(int i = 0; i < array.size(); i++) {
+			sorted_array.add(0);
+		}
+		for(int i = 0; i <= max(array); i++) {
 			temp_array.add(0);
 		}
 		for(int i = 0; i < array.size(); i++) {
-			temp_array.get(array.get(i));
-			temp_array.set(, element)
+			int temp_val = temp_array.get(array.get(i));
+			temp_array.set(array.get(i), ++temp_val);
+		}
+		for(int i = 1; i <= max(array); i++) {
+			int temp_val = temp_array.get(i) + temp_array.get(i-1);
+			temp_array.set(i, temp_val);
+		}
+		for(int i = array.size()-1; i >= 0; i--) {
+			int temp_index = temp_array.get(array.get(i));
+			int temp_val = temp_array.get(array.get(i));
+			sorted_array.set(temp_index-1, array.get(i));
+			temp_array.set(array.get(i), --temp_val);
 		}
 	}
 	
@@ -85,13 +121,13 @@ public class Counting_Sort {
 	// 100라인에 대한 정렬 후 파일 저장 함수
 	public void test100_save() {
 		String path = System.getProperty("user.dir");
-		File file = new File(path + "/src/Resource/Merge_100.txt");
+		File file = new File(path + "/src/Resource/Counting_100.txt");
 		FileWriter writer = null;
 		try {
 			writer = new FileWriter(file, false);
 
-			for (int i = 0; i < array.size(); i++) {
-				writer.write(array.get(i) + "\r\n");
+			for (int i = 0; i < sorted_array.size(); i++) {
+				writer.write(sorted_array.get(i) + "\r\n");
 			}
 			writer.flush();
 		} catch (IOException e) {
@@ -109,13 +145,13 @@ public class Counting_Sort {
 	// 1000라인에 대한 정렬 후 파일 저장 함수
 	public void test1000_save() {
 		String path = System.getProperty("user.dir");
-		File file = new File(path + "/src/Resource/Merge_1000.txt");
+		File file = new File(path + "/src/Resource/Counting_1000.txt");
 		FileWriter writer = null;
 		try {
 			writer = new FileWriter(file, false);
 
-			for (int i = 0; i < array.size(); i++) {
-				writer.write(array.get(i) + "\r\n");
+			for (int i = 0; i < sorted_array.size(); i++) {
+				writer.write(sorted_array.get(i) + "\r\n");
 			}
 			writer.flush();
 		} catch (IOException e) {
--- [Al]03_201604140_박경수_Java/src/Sort/main.java
+++ [Al]03_201604140_박경수_Java/src/Sort/main.java
@@ -1,10 +1,12 @@
 package Sort;
 
+import Algorithm.*;
+
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Counting_Sort CS = new Counting_Sort();
 	}
 
 }
